7fae499f7b3aef6948a7bade0091df18de38056d
Jean <mikitenko@i.ua>
2025-08-17 18:42:25 +0300

    подгрузка ресурсов от версии Prod Dev

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,29 +16,24 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
 
+        YII_ENV_DEV ? '/css/mobile-header.css?v=' . PROJECT_VERSION : 'css/mobile-header.min.css?v=' . PROJECT_VERSION,
+        YII_ENV_DEV ? '/css/style.css?v=' . PROJECT_VERSION : '/css/style.min.css?v=' . PROJECT_VERSION,
+        YII_ENV_DEV ? '/css/widgets.css?v=' . PROJECT_VERSION : '/css/widgets.min.css?v=' . PROJECT_VERSION,
+
         '/vendor/owl-carousel/assets/owl.carousel.min.css?v=' . PROJECT_VERSION,
-
-        '/css/mobile-header.css?v=' . PROJECT_VERSION,
-        '/css/style.min.css?v=' . PROJECT_VERSION,
-        '/css/widgets.css?v=' . PROJECT_VERSION,
-
         '/vendor/fontawesome/css/all.min.css?v=' . PROJECT_VERSION,
 
     ];
     public $js = [
 
+        YII_ENV_DEV ? '/js/number.js?v=' . PROJECT_VERSION : '/js/number.min.js?v=' . PROJECT_VERSION,
+        YII_ENV_DEV ? '/js/header.js?v=' . PROJECT_VERSION : '/js/header.min.js?v=' . PROJECT_VERSION,
+        YII_ENV_DEV ? '/js/main.js?v=' . PROJECT_VERSION : '/js/main.min.js?v=' . PROJECT_VERSION,
+        YII_ENV_DEV ? '/js/collapse.js?v=' . PROJECT_VERSION : '/js/collapse.min.js?v=' . PROJECT_VERSION,
+
         '/vendor/bootstrap/js/bootstrap.bundle.min.js?v=' . PROJECT_VERSION,
         '/vendor/owl-carousel/owl.carousel.min.js?v=' . PROJECT_VERSION,
 
-        
-        '/js/number.js?v=' . PROJECT_VERSION,
-        '/js/header.js?v=' . PROJECT_VERSION,
-        '/js/main.min.js?v=' . PROJECT_VERSION,
-        '/js/collapse.js?v=' . PROJECT_VERSION,
-        
-//        '/vendor/select2/js/select2.min.js?v=' . PROJECT_VERSION,
-//        '/js/ModalRemote.js?v=' . PROJECT_VERSION,
-//        '/js/ajaxcrud.js?v=' . PROJECT_VERSION,
     ];
     public $depends = [
         'yii\web\YiiAsset',
