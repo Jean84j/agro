65b64e4d99588b5ecc09b658f699af0ce519a5a5
Jean <mikitenko@i.ua>
2025-09-06 23:58:50 +0300

    добавил клас для gtm

--- frontend/views/_partials/quickview-button.php
+++ frontend/views/_partials/quickview-button.php
@@ -5,15 +5,13 @@ $urlOpen = Yii::$app->urlManager->createUrl(['quickview/quickview']);
 $number = mt_rand(10, 99);
 
 ?>
-<button class="product-card__quickview"
+<button class="product-card__quickview gtm-quickview"
         type="button"
         id="quickview-<?= $product->id . $number ?>"
         data-product-id="<?= $product->id ?>"
         data-url-quickview="<?= $urlOpen ?>"
         aria-label="Швидкий перегляд"
 >
-    <svg width="16px" height="16px">
-        <use xlink:href="/images/sprite.svg#quickview-16"></use>
-    </svg>
+    <i class="fas fa-expand gtm-quickview" style="font-size: 24px"></i>
     <span class="fake-svg-icon"></span>
 </button>
