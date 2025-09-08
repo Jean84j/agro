1b417dc5c512f982b0f72c2c50cb6ec176afbda5
Jean <mikitenko@i.ua>
2025-09-08 23:32:33 +0300

    ****

--- backend/views/_partials/card-name-label.php
+++ backend/views/_partials/card-name-label.php
@@ -2,7 +2,7 @@
 
 ?>
 <div class="mb-4">
-            <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart">
-                <h2 class="mb-0 fs-exact-18"> <?= Yii::t('app', $cardName) ?></h2>
+            <span class="badge-name_card">
+                <?= Yii::t('app', $cardName) ?>
             </span>
 </div>
