1a9884bd030b666a255ada6e051ce60a3cd0ba62
Jean <mikitenko@i.ua>
2025-09-08 13:18:07 +0300

    **** цена валюта

--- backend/views/product/sidebar/home-content.php
+++ backend/views/product/sidebar/home-content.php
@@ -8,11 +8,11 @@ use yii\helpers\ArrayHelper;
 use yii\widgets\Pjax;
 
 ?>
-<div class="card">
+<div class="card w-100">
     <div class="card-body p-5">
         <div class="mb-5">
                 <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"> <h2
-                        class="mb-0 fs-exact-18"><?= Yii::t('app', 'Category') ?></h2></span>
+                            class="mb-0 fs-exact-18"><?= Yii::t('app', 'Category') ?></h2></span>
         </div>
         <div class="card card-body">
             <?php
@@ -45,7 +45,7 @@ use yii\widgets\Pjax;
     <div class="card-body p-5">
         <div class="mb-5">
                 <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2
-                        class="mb-0 fs-exact-18"><?= Yii::t('app', 'Status') ?></h2></span>
+                            class="mb-0 fs-exact-18"><?= Yii::t('app', 'Status') ?></h2></span>
         </div>
         <div class="card card-body mb-4">
             <?= $form->field($model, 'status_id')
@@ -69,7 +69,7 @@ use yii\widgets\Pjax;
     <div class="card-body p-5">
         <div class="mb-5">
                 <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2
-                        class="mb-0 fs-exact-18"><?= Yii::t('app', 'Package') ?></h2></span>
+                            class="mb-0 fs-exact-18"><?= Yii::t('app', 'Package') ?></h2></span>
         </div>
         <div class="card card-body mb-4">
             <?= $form->field($model, 'package')
@@ -93,49 +93,66 @@ use yii\widgets\Pjax;
     </div>
 </div>
 <div class="card w-100 mt-5">
-    <div class="card-body p-5">
-        <div class="mb-5">
+    <div class="card-body">
+        <div>
                         <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2
-                                class="mb-0 fs-exact-18"><?= Yii::t('app', 'Prices') ?></h2></span>
+                                    class="mb-0 fs-exact-18"><?= Yii::t('app', 'Prices') ?></h2></span>
         </div>
-        <div class="card card-body g-4">
-            <div>
-                <?= $form->field($model, 'price')->textInput([
-                    'class' => "form-control"
-                ]) ?>
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="price-tab-2" data-bs-toggle="tab"
+                            data-bs-target="#price-tab-content-2" type="button" role="tab"
+                            aria-controls="price-tab-content-2" aria-selected="true">
+                        Ціна<span class="nav-link-sa-indicator"></span>
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="currency-tab-2" data-bs-toggle="tab"
+                            data-bs-target="#currency-tab-content-2" type="button" role="tab"
+                            aria-controls="currency-tab-content-2" aria-selected="true">
+                        Валюта<span class="nav-link-sa-indicator"></span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <div class="tab-pane fade show active" id="price-tab-content-2" role="tabpanel"
+                 aria-labelledby="price-tab-2">
+                <div class="card card-body mt-4">
+                    <div>
+                        <?= $form->field($model, 'price')->textInput([
+                            'class' => "form-control"
+                        ]) ?>
+                    </div>
+                    <div>
+                        <?= $form->field($model, 'old_price')->textInput([
+                            'class' => "form-control"
+                        ]) ?>
+                    </div>
+                </div>
             </div>
-            <div>
-                <?= $form->field($model, 'old_price')->textInput([
-                    'class' => "form-control"
-                ]) ?>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card w-100 mt-5">
-    <div class="card-body p-5">
-        <div class="mb-5">
-                <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2
-                        class="mb-0 fs-exact-18"><?= Yii::t('app', 'Currency') ?></h2></span>
-        </div>
-        <div class="card card-body mb-4">
-            <?= $form->field($model, 'currency')
-                ->radioList(
-                    [
-                        'UAH' => 'Гривня',
-                        'USD' => 'Долар',
-                    ],
-                    [
-                        'item' => function ($index, $label, $name, $checked, $value) {
-                            $return = '<label class="form-check">';
-                            $return .= '<input class="form-check-input" type="radio" name="' . $name . '" value="' . $value . '" ' . ($checked ? "checked" : "") . '>';
-                            $return .= ucwords($label);
-                            $return .= '</label>';
-                            return $return;
-                        },
+            <div class="tab-pane fade" id="currency-tab-content-2" role="tabpanel" aria-labelledby="currency-tab-2">
+                <div class="card card-body mt-4">
+                    <?= $form->field($model, 'currency')
+                        ->radioList(
+                            [
+                                'UAH' => 'Гривня',
+                                'USD' => 'Долар',
+                            ],
+                            [
+                                'item' => function ($index, $label, $name, $checked, $value) {
+                                    $return = '<label class="form-check">';
+                                    $return .= '<input class="form-check-input" type="radio" name="' . $name . '" value="' . $value . '" ' . ($checked ? "checked" : "") . '>';
+                                    $return .= ucwords($label);
+                                    $return .= '</label>';
+                                    return $return;
+                                },
 
-                    ],
-                )->label(false); ?>
+                            ],
+                        )->label(false); ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -143,7 +160,7 @@ use yii\widgets\Pjax;
     <div class="card-body p-5">
         <div class="mb-5">
                 <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2
-                        class="mb-0 fs-exact-18"><?= Yii::t('app', 'Brand') ?></h2></span>
+                            class="mb-0 fs-exact-18"><?= Yii::t('app', 'Brand') ?></h2></span>
         </div>
         <div class="card card-body ">
             <?php
