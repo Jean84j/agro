7051965c8f5cab13963f851b7202ea3875ae843f
Jean <mikitenko@i.ua>
2025-09-08 12:08:00 +0300

    **** длинна описаний

--- backend/config/params.php
+++ backend/config/params.php
@@ -5,7 +5,7 @@ return [
     'bsVersion' => '5.x',
     'seoRules' => [
         'seo_title' => ['min' => 50, 'optimal' => 55, 'max' => 60],
-        'seo_description' => ['min' => 140, 'optimal' => 150, 'max' => 160],
+        'seo_description' => ['min' => 130, 'optimal' => 140, 'max' => 150],
         'seo_h1' => ['min' => 30, 'optimal' => 50, 'max' => 70],
     ],
 ];
