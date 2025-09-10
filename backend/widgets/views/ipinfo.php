b68af08735af8e31be4183983eccd5f3989285b3
Jean <mikitenko@i.ua>
2025-09-10 23:25:40 +0300

    **** custom Ip Info

--- backend/widgets/views/ipinfo.php
+++ backend/widgets/views/ipinfo.php
@@ -8,9 +8,8 @@ $countryCode = strtolower($data['country'] ?? '');
 ?>
 <?php if ($countryCode): ?>
     <span class="flag-icon flag-icon-<?= $countryCode ?>"></span>
-    <span><?= htmlspecialchars($data['country']) ?></span>
-<p><?= htmlspecialchars($data['ip'] ?? '') ?></p>
+    <span><?= htmlspecialchars($data['city']) ?></span>
+    <p><?= htmlspecialchars($data['ip'] ?? '') ?></p>
 <?php else: ?>
-    <span class="flag-icon flag-icon"></span>
     <p><?= htmlspecialchars($data['ip'] ?? '') ?></p>
 <?php endif; ?>
