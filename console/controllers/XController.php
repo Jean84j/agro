89cc3beca9df91ddce1188a5c113be36c620aa2d
Jean <mikitenko@i.ua>
2025-12-06 13:30:06 +0200

    ****

--- console/controllers/XController.php
+++ console/controllers/XController.php
@@ -223,7 +223,8 @@ class XController extends Controller
     public function actionFooterDescriptionSub()
     {
 
-        $string = '<p>---------------------------</p>';
+        $string = '<p>---------------------------
+                                                        </p>';
 
 //        $products = Product::find()
 //            ->select(['id', 'footer_description'])
