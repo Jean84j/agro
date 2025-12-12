1c21b038598d9c3d91000e0d3d83a1c6d11e0c68
Jean <mikitenko@i.ua>
2025-12-12 16:43:46 +0200

    facebook icon *

--- backend/views/active-pages/index.php
+++ backend/views/active-pages/index.php
@@ -129,7 +129,7 @@ $ipAddress = Yii::$app->request->getUserIP();
                                     $url === 'https://www.bing.com/' =>
                                     '<i class="fab fa-microsoft microsoft-multicolor-icon"></i>',
 
-                                    $url === 'https://www.bing.com/' =>
+                                    $url === 'https://l.instagram.com/' =>
                                     '<i class="fa-brands fa-instagram"></i>',
 
                                     str_contains($url, 'https://www.google.com/url') =>
