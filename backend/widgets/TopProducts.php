ef22716c43c5565aeee224a6618cfbbee850b409
Jean <mikitenko@i.ua>
2025-12-21 15:43:17 +0200

    widget Top products count views *

--- backend/widgets/TopProducts.php
+++ backend/widgets/TopProducts.php
@@ -4,7 +4,6 @@ namespace backend\widgets;
 
 use app\widgets\BaseWidgetBackend;
 use backend\models\ProductsBackend;
-use yii\helpers\ArrayHelper;
 
 class TopProducts extends BaseWidgetBackend
 {
@@ -17,34 +16,23 @@ class TopProducts extends BaseWidgetBackend
 
     public function run()
     {
-        $uniqueUrls = $this->getProductUniqueUrls();
-
-        ArrayHelper::multisort($uniqueUrls, ['count'], [SORT_DESC]);
-
-        $results = array_slice($uniqueUrls, 0, 10);
-
-        foreach ($results as &$result) {
-            $productData = ProductsBackend::find()
-                ->alias('p')
-                ->select([
-                    'p.name',
-                    'p.id',
-                    'pi.extra_small',
-                ])
-                ->leftJoin('product_image pi', 'pi.product_id = p.id')
-                ->where(['p.slug' => $result['slug']])
-                ->orderBy(['pi.priority' => SORT_ASC])
-                ->asArray()
-                ->one();
-
-            if ($productData) {
-                $result['name'] = $productData['name'];
-                $result['image'] = $productData['extra_small'];
-            }
-        }
+        $productsData = ProductsBackend::find()
+            ->alias('p')
+            ->select([
+                'p.name',
+                'p.id',
+                'p.slug',
+                'p.views AS count',
+                'pi.extra_small AS image',
+            ])
+            ->leftJoin('product_image pi', 'pi.product_id = p.id')
+            ->orderBy(['p.views' => SORT_DESC])
+            ->limit(10)
+            ->asArray()
+            ->all();
 
         return $this->render('recent-activity', [
-            'results' => $results,
+            'results' => $productsData,
             'catalog' => 'product',
             'catalogImage' => 'product',
             'titleWidget' => 'ТОП 10 переглянутих товарів',
