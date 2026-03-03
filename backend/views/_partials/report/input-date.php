7ed0ea473a141c1d569ed4f2a97d76f90f26dd4a
Jean <mikitenko@i.ua>
2026-03-03 11:14:51 +0200

    убрал проверку даты *

--- backend/views/_partials/report/input-date.php
+++ backend/views/_partials/report/input-date.php
@@ -1,5 +1,5 @@
 <?php
-
+//dd($periodStart, $periodEnd);
 ?>
 <form action="<?= $action ?>" method="get">
     <div class="row mb-3">
@@ -7,7 +7,9 @@
         <div class="col-sm-7">
             <input type="date" class="form-control" id="periodStart" name="periodStart"
                    value="<?= htmlspecialchars($periodStart) ?>"
-                   max="<?= htmlspecialchars($periodEnd) ?>" required>
+                   max="
+<!--                   --><?//= htmlspecialchars($periodEnd) ?>
+" required>
         </div>
     </div>
     <div class="row mb-3">
@@ -15,7 +17,9 @@
         <div class="col-sm-7">
             <input type="date" class="form-control" id="periodEnd" name="periodEnd"
                    value="<?= htmlspecialchars($periodEnd) ?>"
-                   min="<?= htmlspecialchars($periodStart) ?>" required>
+                   min="
+<!--                   --><?//= htmlspecialchars($periodStart) ?>
+" required>
         </div>
     </div>
     <?php if (isset($budget)) : ?>
