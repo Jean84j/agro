1a0b371c547e47b7d97f13b8367d3f57bafea7ff
Jean <mikitenko@i.ua>
2026-03-21 19:58:53 +0200

    отзывы *

--- console/controllers/ReviewController.php
+++ console/controllers/ReviewController.php
@@ -4,64 +4,10 @@ namespace console\controllers;
 
 use common\models\shop\Product;
 use common\models\shop\Review;
-use Faker\Factory;
-use Yii;
+use yii\console\Controller;
 
-class ReviewController extends \yii\console\Controller
+class ReviewController extends Controller
 {
-
-    /**
-     * Генерация отзывов
-     */
-    public function actionGenerate()
-    {
-        $users = Yii::$app->db->createCommand('SELECT `second_name` FROM users_review_test')
-            ->queryAll();
-        $products = Product::find()->all();
-        foreach ($users as $user) {
-            if ($user['second_name'] != '') {
-                foreach ($products as $product) {
-                    $rand = rand(3, 5);
-                    $review = new Review();
-                    $review->product_id = $product->id;
-                    $review->rating = $rand;
-                    $review->name = $user['second_name'];
-                    $review->save(false);
-                }
-            }
-        }
-    }
-
-    /**
-     * Добавление отзывов в магазин продукту
-     */
-    public function actionReviewsUser()
-    {
-        $faker = Factory::create('uk_UA');
-        $products = Product::find()->all();
-        $i = 1;
-        foreach ($products as $product) {
-            $users = Yii::$app->db->createCommand('SELECT `second_name` FROM users_review_test')
-                ->queryAll();
-            $user_ramd = rand(1, count($users));
-            $rating = rand(1, 5);
-
-            if (count($product->reviews) <= 25) {
-                if (isset($users[$user_ramd])) {
-                    $review = new Review();
-                    $review->product_id = $product->id;
-                    $review->created_at = strtotime('-' . $i . ' day');
-                    $review->rating = $rating;
-                    $review->name = $users[$user_ramd]['second_name'];
-                    $review->message = $faker->realText(255);
-                    if ($review->save(false)) {
-                        echo "\t" . $i . " Отзыв добавлен! \n";
-                        $i++;
-                    }
-                }
-            }
-        }
-    }
 
     /**
      * Добавление отзывов в магазин AgroPro продуктам без отзывов
@@ -107,8 +53,8 @@ class ReviewController extends \yii\console\Controller
             'Надійне рішення проблеми.', 'надійний продавець', 'працює відмінно',
         ];
 
-        $minTimestamp = 1694592461;
         $maxTimestamp = time();
+        $minTimestamp = strtotime('-7 months');
 
         $products = Product::find()->select('id')->all();
 
