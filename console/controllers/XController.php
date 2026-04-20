d7d795b92bec8a73eec3cefac98b30f4c28974c3
Jean <mikitenko@i.ua>
2026-04-20 12:07:31 +0300

    *

--- console/controllers/XController.php
+++ console/controllers/XController.php
@@ -337,52 +337,6 @@ class XController extends Controller
         foreach ($ips as $ip) {
             Console::output("\t\n ❌ [IP: {$ip['ip_user']}]  Views: {$ip['count']}");
         }
-
-    }
-
-
-    /**
-     * 888888888888888888888888888
-     */
-    public function actionParserIp()
-    {
-        $limit = null;
-
-        $exclude = ['/search/', '/cart/', '/order/'];
-
-        $botIps = IpBot::find()->select('ip')->where(['blocking' => 1])->column();
-
-        $usersIp = ActivePages::find()
-            ->where(['status_serv' => '200'])
-            ->andWhere(['not like', 'url_page', $exclude])
-            ->limit($limit)
-            ->orderBy(['date_visit' => SORT_DESC])
-            ->asArray()
-            ->all();
-
-        $deleteId = [];
-        foreach ($botIps as $botIp) {
-            foreach ($usersIp as $userIp) {
-                if (str_contains($userIp['ip_user'], $botIp)) {
-
-                    $deleteId[] = [
-                        'userId' => $userIp['id'],
-                        'userIp' => $userIp['ip_user'],
-                        'botIp' => $botIp,
-                    ];
-                }
-            }
-
-        }
-
-        $i = 1;
-        foreach ($deleteId as $item) {
-//            if ($url->delete()) {
-            Console::output("\n {$i} \t  ❌ [IP: {$item['userIp']}]  «{$item['botIp']}»: Id: {$item['userId']}");
-//            }
-            $i++;
-        }
-
     }
 
 }
