a82539a765166619e4402e3b797d88b2e3932d5c
Jean <mikitenko@i.ua>
2026-08-06 11:30:18 +0300

    *

--- frontend/views/quickview/quickview.php
+++ frontend/views/quickview/quickview.php
@@ -57,23 +57,7 @@ use yii\helpers\Url;
                 </ul>
             </div>
             <div class="product__sidebar">
-                <div class="product__availability"
-                     style="font-size: 1.5rem; font-weight: 600; letter-spacing: 1px;">
-                    <?php
-                    $statuses = [
-                        1 => ['icon' => 'fas fa-check', 'color' => '#28a745'],
-                        2 => ['icon' => 'fas fa-ban', 'color' => '#ff0000'],
-                        3 => ['icon' => 'fas fa-truck', 'color' => '#ff8300'],
-                        4 => ['icon' => 'fa fa-bars', 'color' => '#0331fc'],
-                    ];
-
-                    $status = $statuses[$product->status_id] ?? ['icon' => '', 'color' => '#060505'];
-                    $statusIcon = $status['icon'] ? '<i style="margin: 5px; color: ' . $status['color'] . ';" class="' . $status['icon'] . '"></i>' : '';
-                    $statusStyle = 'color: ' . $status['color'] . ';';
-
-                    echo $statusIcon . '<span style="' . $statusStyle . '">' . Yii::t('app', $product->status->name) . '</span>';
-                    ?>
-                </div>
+                <?= $this->render('/_partials/status', ['product' => $product]) ?>
                 <div class="product__prices">
                     <?= Yii::$app->formatter->asCurrency($product->getPrice()) ?>
                 </div>
@@ -81,51 +65,7 @@ use yii\helpers\Url;
                     <div class="form-group product__option">
                         <div class="product__actions">
                             <div class="product__actions-item product__actions-item--addtocart">
-                                <?php
-                                $isAvailable = $product->status_id != 2;
-                                $buttonClass = $isAvailable ? 'btn btn-primary product-card__addtocart' : 'btn btn-secondary product-card__addtocart';
-                                $buttonText = $isAvailable
-                                    ? (!$product->getIssetToCart($product->id) ? Yii::t('app', 'Купити') : Yii::t('app', 'В кошику'))
-                                    : Yii::t('app', 'Купити');
-                                ?>
-                                <button class="<?= $buttonClass ?> btn-lg"
-                                        type="button"
-                                        id="<?= $isAvailable ? 'add-to-cart-' . $product->id : '' ?>"
-                                        data-status-btn="<?= Yii::t('app', 'В кошику') ?>"
-                                        data-default-btn="<?= Yii::t('app', 'Купити') ?>"
-                                        data-product-id="<?= $product->id ?>"
-                                        data-url-cart-view="<?= $isAvailable ? Yii::$app->urlManager->createUrl(['cart/cart-view']) : '' ?>"
-                                        data-url-qty-cart="<?= $isAvailable ? Yii::$app->urlManager->createUrl(['cart/qty-cart']) : '' ?>"
-                                    <?= $isAvailable ? '' : 'disabled' ?>>
-                                    <svg width="20px" height="20px" style="display: unset;">
-                                        <use xlink:href="/images/sprite.svg#cart-20"></use>
-                                    </svg>
-                                    <?= $buttonText ?>
-                                </button>
-                            </div>
-                            <div class="product__actions-item product__actions-item--wishlist">
-                                <button type="button" class="btn btn-secondary btn-svg-icon btn-lg product-card__wish"
-                                        data-toggle="tooltip"
-                                        title="Wishlist"
-                                        id="add-from-wish-btn-<?= $product->id ?>"
-                                        data-url-wish="<?= Yii::$app->urlManager->createUrl(['wish/add-to-wish']) ?>"
-                                        data-wish-product-id="<?= $product->id ?>">
-                                    <svg width="16px" height="16px">
-                                        <use xlink:href="/images/sprite.svg#wishlist-16"></use>
-                                    </svg>
-                                </button>
-                            </div>
-                            <div class="product__actions-item product__actions-item--compare">
-                                <button type="button" class="btn btn-secondary btn-svg-icon btn-lg product-card__compare"
-                                        data-toggle="tooltip"
-                                        title="Compare"
-                                        id="add-from-compare-btn-<?= $product->id ?>"
-                                        data-url-compare="<?= Yii::$app->urlManager->createUrl(['compare/add-to-compare']) ?>"
-                                        data-compare-product-id="<?= $product->id ?>">
-                                    <svg width="16px" height="16px">
-                                        <use xlink:href="/images/sprite.svg#compare-16"></use>
-                                    </svg>
-                                </button>
+                                <?= $this->render('/_partials/add-to-cart-button', ['product' => $product]) ?>
                             </div>
                         </div>
                     </div>
