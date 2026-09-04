9eb3ac882a4e1fda6da80ab7b10bfce17112d1d5
Jean <mikitenko@i.ua>
2026-09-04 09:11:59 +0300

    запись переменных в файл

--- func.php
+++ func.php
@@ -1,5 +1,20 @@
 <?php
+
+use yii\helpers\VarDumper;
+
 function debug($arr)
 {
-    \yii\helpers\VarDumper::dump($arr, 10, true);
+    VarDumper::dump($arr, 10, true);
+}
+
+// **** Запись переменных в файл
+function debugInFile($data)
+{
+    $filePath = Yii::getAlias('@runtime/debug_ajax.txt');
+
+    $newEntry = "[" . date('Y-m-d H:i:s') . "]\n" . print_r($data, true) . "\n-------------------\n";
+
+    $existingContent = file_exists($filePath) ? file_get_contents($filePath) : '';
+
+    file_put_contents($filePath, $newEntry . $existingContent);
 }
