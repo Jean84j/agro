346e90371aedab7fc5790158bc5d8c30f614205a
Jean <mikitenko@i.ua>
2026-09-04 09:16:37 +0300

    запись переменных в файл *

--- func.php
+++ func.php
@@ -8,13 +8,20 @@ function debug($arr)
 }
 
 // **** Запись переменных в файл
-function debugInFile($data)
+function debugInFile($data, $maxKilobytes = 500)
 {
     $filePath = Yii::getAlias('@runtime/debug_ajax.txt');
 
     $newEntry = "[" . date('Y-m-d H:i:s') . "]\n" . print_r($data, true) . "\n-------------------\n";
 
     $existingContent = file_exists($filePath) ? file_get_contents($filePath) : '';
+    $combinedContent = $newEntry . $existingContent;
 
-    file_put_contents($filePath, $newEntry . $existingContent);
+    $maxBytes = $maxKilobytes * 1024;
+
+    if (mb_strlen($combinedContent, '8bit') > $maxBytes) {
+        $combinedContent = mb_strcut($combinedContent, 0, $maxBytes, '8bit');
+    }
+
+    file_put_contents($filePath, $combinedContent);
 }
