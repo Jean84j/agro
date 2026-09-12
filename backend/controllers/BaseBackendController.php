206c365f59f6900721a1a74df5e527507558561e
Jean <mikitenko@i.ua>
2026-09-12 12:49:40 +0300

    управление ролями

--- backend/controllers/BaseBackendController.php
+++ backend/controllers/BaseBackendController.php
@@ -2,6 +2,7 @@
 
 namespace backend\controllers;
 
+use Yii;
 use yii\web\Controller;
 
 /**
@@ -9,5 +10,23 @@ use yii\web\Controller;
  */
 class BaseBackendController extends Controller
 {
+    public function beforeAction($action)
+    {
+        // Разрешаем страницу входа и страницу ошибки
+        if (in_array($action->id, ['login', 'error'], true)) {
+            return parent::beforeAction($action);
+        }
 
+        // Не авторизован
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/site/login']);
+        }
+
+        // Нет прав на backend
+        if (!in_array((int)Yii::$app->user->identity->role, [2, 3], true)) {
+            return $this->redirect(['/site/login']);
+        }
+
+        return parent::beforeAction($action);
+    }
 }
